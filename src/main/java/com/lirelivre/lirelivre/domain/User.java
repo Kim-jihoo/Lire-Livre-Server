2f8a792f3f1a3fed1dadfba3e9bed83a029bb050
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-09-21 23:30:42 +0900

    feat: User domain 구현
    ERD를 기반으로 user domain을 구현함.
    
    related to: #8

---
+++ src/main/java/com/lirelivre/lirelivre/domain/User.java
@@ -0,0 +1,51 @@
+package com.lirelivre.lirelivre.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_code")
+	private long userCode;
+
+	@Column(name = "user_id", nullable = false)
+	private String userId;
+
+	@Column(name = "user_name", nullable = false)
+	private String userName;
+
+	@Column(name = "user_password", nullable = false)
+	private String userPassword;
+
+	@Column(name = "user_nickname", nullable = false)
+	private String userNickName;
+
+	@Column(name = "user_email", nullable = false)
+	private String userEmail;
+
+	@Column(name = "user_birthday")
+	private LocalDate userBirthDay;
+
+	@Column(name = "user_gender")
+	private String userGender;
+
+	@Column(name = "user_address")
+	private String userAddress;
+
+	@CreatedDate
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+}
