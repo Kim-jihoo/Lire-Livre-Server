5c1b1f83901c998381ac998a9af44e283463249c
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-09-21 23:32:42 +0900

    style: single class import로 변경
    class import 시 와일드 카드(*)를 single class import로 변경함.
    
    related to: #8
    ref: https://stackoverflow.com/questions/147454/why-is-using-a-wild-card-with-a-java-import-statement-bad

--- src/main/java/com/lirelivre/lirelivre/domain/User.java
+++ src/main/java/com/lirelivre/lirelivre/domain/User.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
