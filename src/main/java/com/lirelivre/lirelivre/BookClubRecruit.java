c5966144de1f5dbb723972a30d225075f40dfeac
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-09-22 09:52:34 +0900

    feat: BookClubRecruit domain 구현
    ERD를 기반으로 BookClubRecruit domain을 구현함.
    
    related to: #8

---
+++ src/main/java/com/lirelivre/lirelivre/BookClubRecruit.java
@@ -0,0 +1,41 @@
+package com.lirelivre.lirelivre;
+
+import com.lirelivre.lirelivre.domain.BookClub;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import java.util.Date;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookClubRecruit {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "club_recruit_id")
+	private long clubRecruitId;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "book_club_id")
+	private BookClub bookClubId;
+
+	@Column(name = "recruit_date")
+	private Date recruitDate;
+
+	@Column(name = "min_num_of_recruit")
+	private int minNumOfRecruit;
+
+	@Column(name = "max_num_of_recruit")
+	private int maxNumOfRecruit;
+}
